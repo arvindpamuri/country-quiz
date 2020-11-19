c98ab05ac50bf4a3031eb44973d4642faa9e514c
arvind pamuri <aravind.pamuri@gmail.com>
2020-11-19 23:02:50 +0530

    complete functionality

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,146 @@
 import React from 'react';
 import Quiz from './Quiz';
 import './Main.css'
+
 class Main extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            countries: [],
-            regions: ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'],
-            quizState: "start"
+            data: [],
+            country_capital: [],
+            quizState: 'start',
+            question: '',
+            answer: '',
+            options: [],
+            buttonDisabled: false,
+            showAnswer: false,
+            score: 0,
+            questionNumber: 1
         }
 
         this.fetchData = this.fetchData.bind(this); 
-        this.loadQuiz = this.loadQuiz.bind(this);
+        this.loadQuestion = this.loadQuestion.bind(this);
+        this.checkAnswer = this.checkAnswer.bind(this);
+        this.retry = this.retry.bind(this);
+        this.next = this.next.bind(this);
     }
 
-    loadQuiz() {
-        this.setState({quizState: "ongoing"})
+    loadQuestion() {
+
+        if (this.state.data.length > 0) {
+            let item = this.state.country_capital[Math.floor(Math.random()*this.state.country_capital.length)]
+            
+            let question = item.name;
+            let answer = item.capital;
+
+            let options_set = new Set();
+            let main_entry = {};
+            main_entry.capital = answer;
+            main_entry.value = true;
+            options_set.add(main_entry)
+            while(options_set.size < 4) {
+                let option_entry = {};
+
+                option_entry.capital = this.state.country_capital[Math.floor(Math.random()*this.state.country_capital.length)].capital;
+
+                option_entry.value = false;
+
+                options_set.add(option_entry)
+            }
+
+            let options = [...options_set];
+
+            //fischer-yates algorithm for random shuffle
+            var i = options.length, k , temp;
+            while(--i > 0){
+                k = Math.floor(Math.random() * (i+1));
+                temp = options[k];
+                options[k] = options[i];
+                options[i] = temp;
+            }
+
+            this.setState({
+                question: question,
+                answer: answer,
+                options: [...options]
+            })
+        }
+        
+        this.setState({quizState: 'ongoing'})
+    }
+
+    checkAnswer(ans) {
+
+        if(ans === 'false') {
+            this.setState({
+                buttonDisabled: true,
+                showAnswer: true,
+            })
+        }   
+        else {
+            this.setState({
+                score: this.state.score + 1,
+                buttonDisabled: true,
+                showAnswer: true,
+            })
+        }
+    }
+
+    next() {
+
+        if(this.state.questionNumber !== 10) {
+            this.setState({
+                buttonDisabled: false,
+                showAnswer: false,
+                questionNumber: this.state.questionNumber + 1
+            })
+            this.loadQuestion()
+        }
+
+        else {
+            this.setState({
+                quizState: 'end',
+                questionNumber: 1,
+            })
+        }
+    }
+
+    retry() {
+        console.log(this.state)
+        this.setState = ({
+            data: [],
+            country_capital: [],
+            quizState: 'start',
+            question: '',
+            answer: '',
+            options: [],
+            buttonDisabled: false,
+            showAnswer: false,
+            score: 0,
+            questionNumber: 1
+        })
     }
 
     fetchData() {
         fetch('https://restcountries.eu/rest/v2/all')
         .then((response) => response.json())
         .then((data) => {
-            let country_names = data.map(item => item.name)
             this.setState({
-                countries: country_names
-              }); 
+                data: data
+            })
+        })
+        .then(() => {
+            let lst = this.state.data.map((item) => {
+                var obj = {
+                    name: item.name,
+                    capital: item.capital
+                }
+                return obj;
+            })
+            this.setState({
+                country_capital: lst
+            })
         })
     }
 
@@ -40,7 +154,16 @@ class Main extends React.Component {
             <div className="play-area">
                 <h1>Country Quiz</h1>
                 <Quiz quizState={this.state.quizState}
-                    loadQuiz = {this.loadQuiz}
+                    loadQuestion = {this.loadQuestion}
+                    question = {this.state.question}
+                    options = {this.state.options}
+                    checkAnswer = {this.checkAnswer}
+                    disabled = {this.state.buttonDisabled}
+                    showAnswer = {this.state.showAnswer}
+                    score = {this.state.score}
+                    retry = {this.retry}
+                    questionNumber = {this.state.questionNumber}
+                    next = {this.next}
                 />
             </div>
         );
--- src/Quiz.js
+++ src/Quiz.js
@@ -2,43 +2,104 @@ import React from 'react';
 import  './Quiz.css';
 import quizlogo from './quizlogo.svg';
 
+const Start = ({loadQuestion}) => {
+    return(
+        <div className="quiz-start-area"> 
+            <section>
+                <img src={quizlogo} alt="logo"/>
+                <h4>Test your knowledge on countries! It contains 10 questions.</h4>
+            </section>
+            <section>
+                <button className="nav-button" onClick={loadQuestion}>start</button>
+            </section>
+        </div>
+    );
+}
+
 class Quiz extends React.Component {
 
     constructor(props) {
         super(props);
+        
+        this.handleOption = this.handleOption.bind(this)
+        this.handleNext = this.handleNext.bind(this)
+        this.handleRetry = this.handleRetry.bind(this)
+    }
 
+    handleOption(el) {
+        this.props.checkAnswer(el.target.value)
+    }
+
+    handleRetry() {
+        setTimeout(() => { this.props.retry(); }, 200);
+    }
+
+    handleNext() {
+        this.props.next()
     }
 
     render() {
 
         if (this.props.quizState === "start") {
             return(
-                <div className="quiz-start-area"> 
-                    <section>
-                        <img src={quizlogo} alt="logo"/>
-                        <h4>Test your knowledge on countries! It contains 10 questions.</h4>
-                    </section>
-                    <section>
-                        <button className="nav-button" onClick={this.props.loadQuiz}>start</button>
-                    </section>
-                </div>
+                <Start loadQuestion={() =>this.props.loadQuestion()}/>
             );
+            
         }
         else if (this.props.quizState === "ongoing") {
+
+            let correctClass = null, wrongClass = '';
+
+
+            if (this.props.showAnswer) {
+                correctClass = 'correct-button'
+                wrongClass = 'wrong-button'
+            }
             return(
                 <div className="quiz-area">
                     <img src={quizlogo} alt="logo" className="top-logo"/>
                     <section>
-                        <h1>what is the capital of</h1>
+                        <h1>
+                            <span>{this.props.questionNumber}.</span>
+                            <span> what is the capital of {this.props.question}</span>
+                        </h1>
                     </section>
                     <section className="button-group">
-                        <button className="option-button">option 1</button>
-                        <button className="option-button">option 2</button>
-                        <button className="option-button">option 3</button>
-                        <button className="option-button">option 4</button>
+                        <button className={`option-button ${(this.props.options[0].value) ? correctClass : wrongClass}`}
+                            onClick={this.handleOption} 
+                            value={this.props.options[0].value}
+                            disabled={this.props.disabled}
+                        >
+                            {this.props.options[0].capital}
+                        </button>
+
+                        <button className={`option-button ${(this.props.options[1].value) ? correctClass : wrongClass}`}
+                            onClick={this.handleOption} 
+                            value={this.props.options[1].value}
+                            disabled={this.props.disabled}
+                        >
+                            {this.props.options[1].capital}
+                        </button>
+
+                        <button className={`option-button ${(this.props.options[2].value) ? correctClass : wrongClass}`}
+                            onClick={this.handleOption} 
+                            value={this.props.options[2].value}
+                            disabled={this.props.disabled}
+                        >
+                            {this.props.options[2].capital}
+                        </button>
+
+                        <button className={`option-button ${(this.props.options[3].value) ? correctClass : wrongClass}`}
+                            onClick={this.handleOption} 
+                            value={this.props.options[3].value}
+                            disabled={this.props.disabled}
+                        >
+                            {this.props.options[3].capital}
+                        </button>
+
                     </section>
                     <section>
-                        <button className="nav-button">Next</button>
+                        <button className="nav-button" onClick={this.handleNext}>Next</button>
                     </section>
                 </div>
             );
@@ -47,10 +108,10 @@ class Quiz extends React.Component {
             return(
                 <div className="quiz-end-area">
                     <section>
-                        <h1>You scored x points</h1>
+                        <h1>You scored {this.props.score} points</h1>
                     </section>
                     <section>
-                        <button className="nav-button">Retry</button>
+                        <button className="nav-button" onClick={this.handleRetry}>Retry?</button>
                     </section>
                 </div>
             );
